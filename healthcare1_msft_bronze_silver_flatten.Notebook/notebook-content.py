f73f9cfed810d9532857deaa3b1bb786c527249f
Vinod Soni <117770765+vinod-soni-microsoft@users.noreply.github.com>
2025-06-16 16:09:58 -0400

    sample data healthcare solution

---
+++ healthcare1_msft_bronze_silver_flatten.Notebook/notebook-content.py
@@ -0,0 +1,98 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "ef170b66-c0bd-4ab5-a913-723b6dccee50",
+# META       "default_lakehouse_name": "healthcare1_msft_silver",
+# META       "default_lakehouse_workspace_id": "6c987fef-a16d-4266-9bc7-0a24340b097e"
+# META     },
+# META     "environment": {
+# META       "environmentId": "c0fc2d9f-118c-9648-477b-cf12b4d4c79b",
+# META       "workspaceId": "00000000-0000-0000-0000-000000000000"
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# ##### WARNING
+# The following notebook is intended to be read only. Please do not modify the contents of this notebook.
+
+
+# CELL ********************
+
+%run healthcare1_msft_config_notebook
+
+# METADATA ********************
+
+# META {
+# META   "frozen": false,
+# META   "editable": false
+# META }
+
+# CELL ********************
+
+%run healthcare1_msft_config_notebook {"enable_spark_setup" : true, "enable_packages_mount" : false}
+
+# METADATA ********************
+
+# META {
+# META   "frozen": false,
+# META   "editable": false
+# META }
+
+# PARAMETERS CELL ********************
+
+inline_params = "{}"
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark",
+# META   "frozen": false,
+# META   "editable": false
+# META }
+
+# CELL ********************
+
+from microsoft.fabric.hls.hds.services.silver_ingestion_service import SilverIngestionService
+import json
+
+# convert inline params into dictionary
+inline_params_dict = json.loads(inline_params)
+
+silver_ingestion_service = SilverIngestionService(
+        spark=spark,
+        workspace_name=workspace_name, 
+        solution_name=solution_name,
+        admin_lakehouse_name=administration_database_name,
+        inline_params=inline_params_dict,
+        one_lake_endpoint=one_lake_endpoint
+)
+
+silver_ingestion_service.run()
+
+# METADATA ********************
+
+# META {
+# META   "frozen": false,
+# META   "editable": false
+# META }
+
+# CELL ********************
+
+mssparkutils.fs.unmount(packages_mount_name)
+
+# METADATA ********************
+
+# META {
+# META   "frozen": false,
+# META   "editable": false
+# META }
