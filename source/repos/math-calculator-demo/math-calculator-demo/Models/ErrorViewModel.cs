f44ab5f3ec4b1fd5165641efbad81982bd99e57b
Vinod Soni <vinod.soni@microsoft.com>
2025-06-19 10:01:24 -0400

    math-calculator app setup

---
+++ source/repos/math-calculator-demo/math-calculator-demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace math_calculator_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
